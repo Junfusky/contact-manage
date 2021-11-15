ce52f06a7a758e45cabf284a2da6ba7b5f9062e5
Yanting Yonna Wu <yantiwu@tesla.com>
2021-11-15 07:05:47 -0800

    added contact-manager module

---
+++ app/src/app/contact-manager/contact-manager-app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-manager-app',
+  template: `
+    <p>
+      contact-manager-app works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ContactManagerAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
