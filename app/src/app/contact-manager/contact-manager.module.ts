bac5ae82930403276ce8003a314076e5db3997d1
Yonna <MXMXJUN@GMAIL.COM>
2021-12-05 11:23:26 -0500

    display data in table with pagination and sorting

--- app/src/app/contact-manager/contact-manager.module.ts
+++ app/src/app/contact-manager/contact-manager.module.ts
@@ -12,6 +12,7 @@ import { MainContentComponent } from './components/main-content/main-content.com
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { UserService } from './services/user.service';
 import {  HttpClientModule } from '@angular/common/http';
+import { NotesComponent } from './components/notes/notes.component';
 
 const routes: Routes = [
   { path: '', component: ContactManagerAppComponent,
@@ -28,7 +29,8 @@ const routes: Routes = [
     ContactManagerAppComponent,
     ToolbarComponent,
     MainContentComponent,
-    SidenavComponent
+    SidenavComponent,
+    NotesComponent
   ],
   imports: [
     CommonModule,
